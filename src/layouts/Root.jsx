0fc4af81f0598637b37b80c41a0fbbe16464ae71
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-04-16 03:41:36 +0300

    Footer added

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Header/Navbar";
+import Footer from "../components/Footer/Footer";
 
 const Root = () => {
   return (
@@ -8,6 +9,7 @@ const Root = () => {
       <div className="max-w-6xl mx-auto px-4 md:px-3 lg:px-2 font-poppins">
         <Outlet />
       </div>
+      <Footer />
     </>
   );
 };
